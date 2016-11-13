a89edfca700680d70fa92114a5d03721d22a7cd2
harismh <harismh@icloud.com>
2016-11-12 16:44:25 -0800

    added dates to posts

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,17 @@
 // YOUR CODE HERE:
 
-var app = {};
-
-app.data;
-
-app.username = window.location.search.replace(/(&|\?)username=/, '');
-
-app.friends = {};
-
-app.server = 'https://api.parse.com/1/classes/messages';
+var app = {
+  data: undefined,
+  username: window.location.search.replace(/(&|\?)username=/, ''),
+  friends: {},
+  server: 'https://api.parse.com/1/classes/messages'
+};
 
 app.init = () => {
   app.clearMessages();
   app.fetch();
+
+
 };
 
 app.superInit = () => {
@@ -67,31 +66,6 @@ app.fetch = () => {
   });  
 };
 
-app.superFetch = () => {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    type: 'GET',
-    url: 'https://api.parse.com/1/classes/messages',
-    data: 'order=-createdAt',
-    success: function (data) {
-      console.log('chatterbox: Fetch successful');
-      var data = data.results;
-      app.data = data;
-      var rooms = {};
-      for (var i = 0; i < data.length; i++) {
-        app.renderMessage(data[i]);
-        rooms[data[i].roomname] = data[i].roomname;
-      }
-      for (var key in rooms) {
-        $('#room-list').append(`<option>${rooms[key]}</option>`);
-      }
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Fetch failed', data);
-    }
-  }); 
-};
 
 app.clearMessages = () => {
   $('#chats').html('');
@@ -118,14 +92,6 @@ app.renderRoom = (string) => {
   $('#roomSelect').append(`<div id=${string}></div>`);
 };
 
-app.superRender = (str) => {
-  app.clearMessages();
-  var data = app.data.filter( message => message.roomname === str);
-  for (var i = 0; i < data.length; i++) {
-    app.renderMessage(data[i]);
-  }
-};
-
 app.handleUsernameClick = function() {
   var username = $(this).text();
   app.friends[username] = username;    
@@ -136,5 +102,95 @@ app.handleSubmit = function () {
   var username = app.username;
   var message = app.createMessage(username, text);
   app.send(message);
+  $('#message-form').val('');
+};
+
+/*************************************************************************/
+/*************************************************************************/
+/******************************SUPER FUNCTIONS****************************/
+/*************************************************************************/
+/*************************************************************************/
+
+
+app.superSend = (message) => {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      app.superRenderMessage(message);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.superFetch = () => {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    type: 'GET',
+    url: 'https://api.parse.com/1/classes/messages',
+    data: 'order=-createdAt',
+    success: function (data) {
+      console.log('chatterbox: Fetch successful');
+      var data = data.results;
+      app.data = data;
+      var rooms = {};
+      for (var i = 0; i < data.length; i++) {
+        app.superRenderMessage(data[i]);
+        rooms[data[i].roomname] = data[i].roomname;
+      }
+      for (var key in rooms) {
+        $('#room-list').append(`<option>${rooms[key]}</option>`);
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Fetch failed', data);
+    }
+  }); 
+};
+
+app.superRenderMessage = (message) => {
+  if (message.text === undefined) {
+    return;
+  }
+
+  var regx = /(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g;
+  if (message.text.match(regx) !== null) {
+    message.text = message.text.replace(regx, 'WARNING MALICIOUS CODE');
+  }
+
+  if (message.username !== app.username) {
+    var regxTime = /(?:[01]\d|2[0123]):(?:[012345]\d)/gm;
+    var time = message.createdAt.match(regxTime);
+  } else {
+    var d = new Date();
+    var time = d.getHours() + ':' + d.getMinutes();
+  }
+
+  var message = `<div id="send" class="chat" ${message.roomname}><div class="username">${message.username}</div><div class="message">${message.text}</div><sub class="time">${time}</sub></div>`;
+
+  $('#chats').prepend(message);
+};
+
+app.superRender = (str) => {
+  app.clearMessages();
+  var data = app.data.filter( message => message.roomname === str);
+  for (var i = 0; i < data.length; i++) {
+    app.renderMessage(data[i]);
+  }
+};
+
+app.superHandleSubmit = function () {
+  var text = $('#message-form').val();
+  var username = app.username;
+  var message = app.createMessage(username, text);
+  app.superSend(message);
   $('#message-form').val('');
 };
--- client/scripts/superInit.js
+++ client/scripts/superInit.js
@@ -1,37 +1,29 @@
 $(document).ready(function() {
 
-  app.init();
+  app.superInit();
 
   $(document).on('click', '#submit-button', function(e) {
     e.preventDefault();
-    app.handleSubmit();
+    app.superHandleSubmit();
   });
 
   $(document).on('click', '#refresh-button', function(e) {
-    app.init();
+    app.superInit();
   });
 
   $(document).on('click', '.username', function(e) {
-    console.log('clicked document .username');
     app.handleUsernameClick.call($(this));
     $(this).parent().addClass('friend');
   });
 
   $('#message-form').keypress(function(e) {
     if (e.which === 13) {
-      app.handleSubmit();
+      app.superHandleSubmit();
     }
   });
 
   $('#room-list').change(function(e) { 
     app.superRender($(this).val());
-  });
-
-  //Mocha Runner Specific
-  $('#send').on('submit', function(e) {
-    console.log('clicked #send');
-    e.preventDefault();
-    app.handleSubmit();
   });
   
 });  
